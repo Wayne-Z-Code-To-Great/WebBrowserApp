3bfd31d98e2273b6c596cb6eb1662ec680fdc130
Wayne-Z-Code-To-Great <60633000+Wayne-Z-Code-To-Great@users.noreply.github.com>
2020-10-29 01:11:18 -0400

    Implement the basic features of go, back, and forward buttons.

--- app/src/main/java/temple/edu/webbrowserapp/BrowserActivity.java
+++ app/src/main/java/temple/edu/webbrowserapp/BrowserActivity.java
@@ -1,14 +1,37 @@
 package temple.edu.webbrowserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class BrowserActivity extends AppCompatActivity {
-
+public class BrowserActivity extends AppCompatActivity implements  PageControlFragment.buttonClickInterface{
+    PageControlFragment controlFragment=new PageControlFragment();
+    PageViewerFragment viewerFragment=new PageViewerFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        transaction.add(R.id.page_control_container, controlFragment, "Url_text");
+        transaction.addToBackStack(null);
+        transaction.commit();
+        FragmentTransaction transaction2=manager.beginTransaction();
+        transaction2.add(R.id.page_viewer_container, viewerFragment);
+        transaction2.addToBackStack(null);
+        transaction2.commit();
+    }
+
+    @Override
+    public void buttonClick(String Url) {
+        viewerFragment.gettingUrl(Url);
+    }
+
+    @Override
+    public void buttonClick(int i) {
+        viewerFragment.forwardOrback(i);
     }
 }
--- app/src/main/java/temple/edu/webbrowserapp/PageViewerFragment.java
+++ app/src/main/java/temple/edu/webbrowserapp/PageViewerFragment.java
@@ -1,5 +1,6 @@
 package temple.edu.webbrowserapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PageViewerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PageViewerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String url;
+    WebView webView;
 
     public PageViewerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PageViewerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PageViewerFragment newInstance(String param1, String param2) {
-        PageViewerFragment fragment = new PageViewerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page_viewer, container, false);
+        View v=inflater.inflate(R.layout.fragment_page_viewer, container, false);
+        webView=(WebView) v.findViewById(R.id.webView);
+        webView.canGoBack();
+        webView.canGoForward();
+        webView.setWebViewClient(new myWebViewClient());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true); //enable javascript
+        webView.loadUrl(url);
+        return v;
+    }
+
+
+
+    private class myWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+    }
+
+    public void gettingUrl(String message) {
+        url=message;
+        webView.loadUrl(url);
+    }
+
+    public void forwardOrback(int condition) {
+        if(condition==1) {
+            webView.goBack();
+        } else {
+            webView.goForward();
+        }
     }
 }
